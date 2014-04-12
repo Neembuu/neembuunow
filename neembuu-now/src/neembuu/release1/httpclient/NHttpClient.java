a9d3b7ddd7346831d335531d24ae3a68ac37eb6b
DavidePastore <pasdavide@gmail.com>
2014-04-12 15:15:37 +0200

    [NHttpClient] Use PoolingClientConnectionManager and allow all https urls.

--- neembuu-now/src/neembuu/release1/httpclient/NHttpClient.java
+++ neembuu-now/src/neembuu/release1/httpclient/NHttpClient.java
@@ -16,7 +16,16 @@
  */
 package neembuu.release1.httpclient;
 
+import java.security.KeyManagementException;
 import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import neembuu.config.GlobalTestSettings;
 import org.apache.http.HttpConnection;
 import org.apache.http.HttpHost;
@@ -24,21 +33,25 @@ import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.AuthState;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.conn.ClientConnectionManager;
 import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
 import org.apache.http.impl.DefaultHttpClientConnection;
 import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.PoolingClientConnectionManager;
 import org.apache.http.impl.conn.SingleClientConnManager;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.ExecutionContext;
 import org.apache.http.protocol.HttpContext;
+import org.openide.util.Exceptions;
 
 /**
  * Neembuu HttpClient.
@@ -59,70 +72,104 @@ public class NHttpClient {
     public static DefaultHttpClient getInstance(){
         //Initializes the instance.
         if(httpClient == null){
-            httpClient = new DefaultHttpClient();
-            GlobalTestSettings.ProxySettings proxySettings 
-                            = GlobalTestSettings.getGlobalProxySettings();
-            HttpContext context = new BasicHttpContext();
-            SchemeRegistry schemeRegistry = new SchemeRegistry();
+            try {
+                GlobalTestSettings.ProxySettings proxySettings
+                        = GlobalTestSettings.getGlobalProxySettings();
+                HttpContext context = new BasicHttpContext();
+                SchemeRegistry schemeRegistry = new SchemeRegistry();
+                
+                schemeRegistry.register(new Scheme("http", new PlainSocketFactory(), 80));
+                
+                try{
+                    KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+                    schemeRegistry.register(new Scheme("https", new SSLSocketFactory(keyStore), 8080));
+                }catch(Exception a){
+                    a.printStackTrace(System.err);
+                }
+                
+                
+                /* Allow all hostname */
+                //SSL http://javaskeleton.blogspot.it/2010/07/avoiding-peer-not-authenticated-with.html
+                SSLContext ctx;
+                ctx = SSLContext.getInstance("TLS");
+                 
+                X509TrustManager tm = new X509TrustManager() {
 
-            schemeRegistry.register(new Scheme("http", new PlainSocketFactory(), 80));
+                    @Override
+                    public void checkClientTrusted(X509Certificate[] xcs, String string) throws CertificateException {
+                    }
 
-            try{
-                KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-                schemeRegistry.register(new Scheme("https", new SSLSocketFactory(keyStore), 8080));
-            }catch(Exception a){
-                a.printStackTrace(System.err);
-            }
+                    @Override
+                    public void checkServerTrusted(X509Certificate[] xcs, String string) throws CertificateException {
+                    }
 
-            context.setAttribute(
-                    ClientContext.SCHEME_REGISTRY,
-                    schemeRegistry);
-            context.setAttribute(
-                    ClientContext.AUTHSCHEME_REGISTRY,
-                    new BasicScheme()/*file.httpClient.getAuthSchemes()*/);
-
-
-            context.setAttribute(
-                    ClientContext.COOKIESPEC_REGISTRY,
-                    httpClient.getCookieSpecs()/*file.httpClient.getCookieSpecs()*/
-                    );
-
-            BasicCookieStore basicCookieStore = new BasicCookieStore();
-
-            context.setAttribute(
-                    ClientContext.COOKIE_STORE,
-                    basicCookieStore/*file.httpClient.getCookieStore()*/);
-            context.setAttribute(
-                    ClientContext.CREDS_PROVIDER,
-                    new BasicCredentialsProvider()/*file.httpClient.getCredentialsProvider()*/);
-
-            HttpConnection hc = new DefaultHttpClientConnection();
-            context.setAttribute(
-                    ExecutionContext.HTTP_CONNECTION,
-                    hc);
-
-            //System.out.println(file.httpClient.getParams().getParameter("http.useragent"));
-            HttpParams httpParams = new BasicHttpParams();
-
-            if(proxySettings!=null){
-                AuthState as = new AuthState();
-                as.setCredentials(new UsernamePasswordCredentials(proxySettings.userName, proxySettings.password));
-                as.setAuthScope(AuthScope.ANY);
-                as.setAuthScheme(new BasicScheme());
-                httpParams.setParameter(ClientContext.PROXY_AUTH_STATE,as);
-                httpParams.setParameter("http.proxy_host", new HttpHost(proxySettings.host, proxySettings.port));
-            }
-
-            httpClient = new DefaultHttpClient(new SingleClientConnManager(
-                    httpParams/*file.httpClient.getParams()*/,
-                    schemeRegistry),
-                    httpParams/*file.httpClient.getParams()*/);
-
-
-            if(proxySettings!=null){
-                httpClient.getCredentialsProvider().setCredentials(
-                    AuthScope.ANY,
-                    new UsernamePasswordCredentials(proxySettings.userName, proxySettings.password));
+                    @Override
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return null;
+                    }
+                };
+                ctx.init(null, new TrustManager[]{tm}, null);
+                SSLSocketFactory ssf = new SSLSocketFactory(ctx);
+                
+                SchemeRegistry registry = new SchemeRegistry();
+                registry.register(new Scheme("http", 80, PlainSocketFactory.getSocketFactory()));
+                registry.register(new Scheme("https", 443, ssf));
+                ClientConnectionManager ccm = new PoolingClientConnectionManager(registry);
+                
+                httpClient = new DefaultHttpClient(ccm);
+                
+                context.setAttribute(
+                        ClientContext.SCHEME_REGISTRY,
+                        schemeRegistry);
+                
+                context.setAttribute(
+                        ClientContext.AUTHSCHEME_REGISTRY,
+                        new BasicScheme()/*file.httpClient.getAuthSchemes()*/
+                );
+                
+                context.setAttribute(
+                        ClientContext.COOKIESPEC_REGISTRY,
+                        httpClient.getCookieSpecs()/*file.httpClient.getCookieSpecs()*/
+                );
+                
+                BasicCookieStore basicCookieStore = new BasicCookieStore();
+                
+                context.setAttribute(
+                        ClientContext.COOKIE_STORE,
+                        basicCookieStore/*file.httpClient.getCookieStore()*/);
+                context.setAttribute(
+                        ClientContext.CREDS_PROVIDER,
+                        new BasicCredentialsProvider()/*file.httpClient.getCredentialsProvider()*/);
+                
+                HttpConnection hc = new DefaultHttpClientConnection();
+                context.setAttribute(
+                        ExecutionContext.HTTP_CONNECTION,
+                        hc);
+                
+                //System.out.println(file.httpClient.getParams().getParameter("http.useragent"));
+                HttpParams httpParams = new BasicHttpParams();
+                
+                if(proxySettings!=null){
+                    AuthState as = new AuthState();
+                    as.setCredentials(new UsernamePasswordCredentials(proxySettings.userName, proxySettings.password));
+                    as.setAuthScope(AuthScope.ANY);
+                    as.setAuthScheme(new BasicScheme());
+                    httpParams.setParameter(ClientContext.PROXY_AUTH_STATE,as);
+                    httpParams.setParameter("http.proxy_host", new HttpHost(proxySettings.host, proxySettings.port));
+                }
+                
+                httpClient.setParams(httpParams /*file.httpClient.getParams()*/);
+                
+                
+                if(proxySettings != null){
+                    httpClient.getCredentialsProvider().setCredentials(
+                            AuthScope.ANY,
+                            new UsernamePasswordCredentials(proxySettings.userName, proxySettings.password));
+                }
+            } catch (NoSuchAlgorithmException ex) {
+                Exceptions.printStackTrace(ex);
+            } catch (KeyManagementException ex) {
+                Exceptions.printStackTrace(ex);
             }
         
         
