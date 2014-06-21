2e8c9151e7ba0eaca2c72e662fd01c158e645737
DavidePastore <pasdavide@gmail.com>
2014-06-21 17:21:57 +0200

    [Facebook] Add link owner.
    Better organization of the code and better documentation.

--- neembuu-facebook/src/main/java/neembuu/release1/facebook/FacebookFeedItem.java
+++ neembuu-facebook/src/main/java/neembuu/release1/facebook/FacebookFeedItem.java
@@ -17,6 +17,8 @@ public class FacebookFeedItem implements FeedItem{
     private String url;
     
     private String imageURL;
+    
+    private String authorName;
 
     public String text() {
         return text;
@@ -30,21 +32,45 @@ public class FacebookFeedItem implements FeedItem{
         return imageURL;
     }
     
+    public String authorName(){
+        return authorName;
+    }
+    
+    /**
+     * Set the text.
+     * @param newText The new text.
+     */
     public void setText(String newText){
         this.text = newText;
     }
     
+    /**
+     * Set the video url.
+     * @param newUrl The new video url.
+     */
     public void setUrl(String newUrl){
         this.url = newUrl;
     }
     
+    /**
+     * Set the image url of the video.
+     * @param newImageUrl The new image url of the video.
+     */
     public void setImageUrl(String newImageUrl){
         this.imageURL = newImageUrl;
+    }
+    
+    /**
+     * Set the author name.
+     * @param newAuthorName The new author name. 
+     */
+    public void setAuthorName(String newAuthorName){
+        this.authorName = newAuthorName;
     }
 
     @Override
     public String toString() {
-        return String.format("Text: %s\nUrl: %s\nImageUrl: %s", text, url, imageURL);
+        return String.format("Name: %s\nText: %s\nUrl: %s\nImageUrl: %s", authorName, text, url, imageURL);
     }
     
     
